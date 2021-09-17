d70d555e99abbf34209ece1967aa1c78b6b73d46
Francesca Bryden <francesca.bryden@dft.gov.uk>
2021-09-17 16:25:03 +0100

    Added warning and default number of levels to gradient function

--- R/extractGradient.R
+++ R/extractGradient.R
@@ -1,11 +1,13 @@
 #' Function to extract a gradient of colours as hex codes
 #'
 #' @param colours Individual names of colours to start and end gradient with, in a vector
-#' @param n Number of colours required in created gradient, as an integer
+#' @param n Number of colours required in created gradient, as an integer. Defaults to 5.
 #' @name extractGradient
 #' @title Extract gradient of n colours as hex codes based on DfT palettes
 #'
-extractGradient <- function(colours, n){
+extractGradient <- function(colours, n = 5){
+  warning("Gradient palettes do not meet accessibility requirements for publishing")
+
   ramp  <- colorRampPalette(colors = dft.gradients[[colours]])
   ramp(n)
 }
