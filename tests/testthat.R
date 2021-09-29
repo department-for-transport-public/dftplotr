1084df6017069c6b9ea551630605b6ec3c5b2235
Francesca Bryden <francesca.bryden@dft.gov.uk>
2021-09-29 08:20:55 +0100

    Added basic test setup

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(dftplotr)
+
+test_check("dftplotr")
---
+++ tests/testthat/test-palettes.R
@@ -0,0 +1,15 @@
+test_that("Colours are a named vector", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("Palettes are in list format", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("All palettes contain at least 4 colours", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("All gradients contain exactly 2 colours", {
+  expect_equal(2 * 2, 4)
+})
