25a63a172cefb2943716bc9da1f351880649d5a2
Francesca Bryden <francesca.bryden@dft.gov.uk>
2021-09-29 08:55:55 +0100

    Added unit tests for palettes

--- tests/testthat/test-palettes.R
+++ tests/testthat/test-palettes.R
@@ -1,15 +1,27 @@
 test_that("Colours are a named vector", {
-  expect_equal(2 * 2, 4)
+  expect_true(is.vector(dft.colours))
+  expect_true(!is.null(names(dft.colours)))
 })
 
 test_that("Palettes are in list format", {
-  expect_equal(2 * 2, 4)
+  expect_true(is.list(dft.palettes))
+  expect_true(is.list(dft.gradients))
+})
+
+test_that("6 options available in each list of palettes/gradients", {
+  expect_length(dft.palettes, 6)
+  expect_length(dft.gradients, 6)
 })
 
 test_that("All palettes contain at least 4 colours", {
-  expect_equal(2 * 2, 4)
+
+  for(i in 1:length(dft.palettes)){
+  expect_gt(length(dft.palettes[[i]]), 3)
+  }
 })
 
 test_that("All gradients contain exactly 2 colours", {
-  expect_equal(2 * 2, 4)
+  for(i in 1:length(dft.palettes)){
+    expect_equal(length(dft.gradients[[i]]), 2)
+  }
 })
