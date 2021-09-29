128eb9f8efceba4c6f85bf0d5f8ec4f091ddfc9b
Francesca Bryden <francesca.bryden@dft.gov.uk>
2021-09-29 13:38:02 +0100

    Combined inner workings into an internal function to simplify code

--- R/scaleXDft.R
+++ R/scaleXDft.R
@@ -1,15 +1,19 @@
-#' Function to use custom DfT palettes for fill colours in ggplot charts
+#' Internal function to select which palette to use in scale_fill_dft and scale_colour_dft and output warnings/errors as appropriate
 #'
-#' @export
-#' @param palette Named DfT palette to use in the fill. Defaults to "main.palette"
-#' @param gradient Boolean response whether to use gradient palette instead of standard palettes. Defaults to false.
+#' @param palette Named DfT palette to use in the fill.
+#' @param gradient Boolean response whether to use gradient palette instead of standard palettes.
 #' @param ... Other arguments to pass to extract gradients function
-#' @name scale_fill_dft
+#' @name scale_all_dft
 #' @title Extract named palette colours as hex codes
-#'
-#'
-scale_fill_dft <- function(palette = "main.palette", gradient = FALSE, ...) {
 
+scale_all_dft <- function(palette, gradient, ...){
+  #Stop and list palette names if palette name is not recognised
+  if(!palette %in% names(dft.palettes)){
+    stop(paste(palette, "is not a recognised DfT palette. Available palettes are: ",
+               paste(names(dft.palettes), collapse = ", ")))
+  }
+
+  ##If a gradient palette is needed:
   if (gradient) {
     ##Create a gradient based on the palette selected
     pal <- unname(extract_gradient(palette = palette, ...))
@@ -20,10 +24,27 @@ scale_fill_dft <- function(palette = "main.palette", gradient = FALSE, ...) {
     } else{
       message("This palette meets WCAG 2.0 AAA accessibility guidelines. This is the highest standard of accessibility.")
     }
-    #Select one of the pre-created palettes
+    #Else, select one of the pre-created palettes
     pal <- unname(dft.palettes[[palette]])
 
   }
+
+  return(pal)
+
+}
+
+#' Function to use custom DfT palettes for fill colours in ggplot charts
+#'
+#' @export
+#' @param palette Named DfT palette to use in the fill. Defaults to "main.palette"
+#' @param gradient Boolean response whether to use gradient palette instead of standard palettes. Defaults to false.
+#' @param ... Other arguments to pass to extract gradients function
+#' @name scale_fill_dft
+#' @title Extract named palette colours as hex codes for use in area charts
+#'
+#'
+scale_fill_dft <- function(palette = "main.palette", gradient = FALSE, ...) {
+  pal <- scale_all_dft(palette = palette, gradient = gradient, ...)
   scale_fill_manual(values = pal)
 }
 
@@ -35,24 +56,11 @@ scale_fill_dft <- function(palette = "main.palette", gradient = FALSE, ...) {
 #' @param gradient Boolean response whether to use gradient palette instead of standard palettes. Defaults to false.
 #' @param ... Other arguments to pass to extract gradients function
 #' @name scale_colour_dft
-#' @title Extract named palette colours as hex codes
+#' @title Extract named palette colours as hex codes for use in line charts
 #'
 #'
 scale_colour_dft <- function(palette = "main.palette", gradient = FALSE, ...) {
 
-  if (gradient) {
-    ##Create a gradient based on the palette selected
-    pal <- unname(extract_gradient(palette = palette, ...))
-  } else {
-    ##Add a message to explain the accessibility of the palette chosen
-    if(palette %in% c("mountain.train", "clear.skies")) {
-      message("This palette meets WCAG 2.0 AA accessibility guidelines only. This is the basic level expected for publication")
-    } else{
-      message("This palette meets WCAG 2.0 AAA accessibility guidelines. This is the highest standard of accessibility.")
-    }
-    #Select one of the pre-created palettes
-    pal <- unname(dft.palettes[[palette]])
-
-  }
+  pal <- scale_all_dft(palette = palette, gradient = gradient, ...)
   scale_color_manual(values = pal)
 }
