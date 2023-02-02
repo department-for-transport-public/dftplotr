253dc1050a69230c326687a715cdb478cfdb5b0d
Fran <francesca.bryden@dft.gov.uk>
2023-02-02 16:05:30 +0000

    Removing plyr dependency

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -90,8 +90,8 @@ number <- function(x, accuracy = NULL, scale = 1, prefix = "",
                    suffix = "", big.mark = " ", decimal.mark = ".",
                    trim = TRUE, ...) {
   if (length(x) == 0) return(character())
-  accuracy <- accuracy %||% precision(x * scale)
-  x <- plyr::round_any(x, accuracy / scale)
+  accuracy <- accuracy %||% scales:::precision(x * scale)
+  x <- scales:::round_any(x, accuracy / scale)
   nsmall <- -floor(log10(accuracy))
   nsmall <- min(max(nsmall, 0), 20)
 
@@ -114,3 +114,40 @@ number <- function(x, accuracy = NULL, scale = 1, prefix = "",
   ret
 }
 
+#' Round to multiple of any number.
+#'
+#' @param x numeric or date-time (POSIXct) vector to round
+#' @param accuracy number to round to; for POSIXct objects, a number of seconds
+#' @param f rounding function: \code{\link{floor}}, \code{\link{ceiling}} or
+#'  \code{\link{round}}
+#' @keywords manip
+#' @export
+#' @examples
+#' round_any(135, 10)
+#' round_any(135, 100)
+#' round_any(135, 25)
+#' round_any(135, 10, floor)
+#' round_any(135, 100, floor)
+#' round_any(135, 25, floor)
+#' round_any(135, 10, ceiling)
+#' round_any(135, 100, ceiling)
+#' round_any(135, 25, ceiling)
+#'
+#' round_any(Sys.time() + 1:10, 5)
+#' round_any(Sys.time() + 1:10, 5, floor)
+#' round_any(Sys.time(), 3600)
+round_any <- function(x, accuracy, f = round) {
+  UseMethod("round_any")
+}
+
+#' @export
+round_any.numeric <- function(x, accuracy, f = round) {
+  f(x / accuracy) * accuracy
+}
+
+#' @export
+round_any.POSIXct <- function(x, accuracy, f = round) {
+  tz <- format(x[1], "%Z")
+  xr <- round_any(as.numeric(x), accuracy, f)
+  as.POSIXct(xr, origin="1970-01-01 00:00.00 UTC", tz=tz)
+}
